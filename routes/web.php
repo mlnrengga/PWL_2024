43338c69c18d45df73e955824d9698eb27937432
Maulana Rengga Ramadan <mlnrengga@gmail.com>
2025-02-18 17:28:36 +0700

    Praktikum_3

--- routes/web.php
+++ routes/web.php
@@ -60,3 +60,9 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([ 
 'create', 'store', 'update', 'destroy' 
 ]); 
+
+// Route::get('/greeting', function () { 
+//     return view('blog.hello', ['name' => 'Rengga']); 
+//     }); 
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']); 
